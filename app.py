d140e9c333af32db69edb2fe517d7ce6ab7d297b
Caua-Carlos <carlos.caua@escolar.ifrn.edu.br>
2026-03-26 12:05:17 +0000

    Adição de novas rota e aplicação de parametros

--- app.py
+++ app.py
@@ -10,7 +10,32 @@ def indedx():
 @app.route('/faleconosco')
 
 def contato():
-    return render_template('contato.html')
+    return render_template('contato.html', nome ="Cauã Carlos", email = "carlos.caua@escolar.ifrn.edu.br", numero = "84 988888888")
+   
+@app.route("/usuario", defaults = {"nome" : "desconhecido", "sobrenome" : "desconhecido"})
+
+@app.route("/usuario/<nome>/<sobrenome>")
+def usuario(nome, sobrenome):
+    return render_template('usuario.html', nome = nome, sobrenome = sobrenome)
+
+@app.route("/semestre", defaults = {"x" : 0 })
+def semesttre(x):
+    atual = x
+    anterior = 0
+    return render_template("semestre.html", atual = atual, anterior = anterior )
+
+@app.route("/semestre/<int:x>")
+def semestre(x):
+    atual = x
+    if atual > 0:
+        anterior = x-1
+        return render_template("semestre.html", atual = atual, anterior = anterior)
+    elif atual <= 0:
+        anterior = "Não há semestre anterior"
+        return render_template("semestre.html", atual = atual, anterior = anterior )
+    
+
+
 
 if __name__ == '__main__':
     app.run()
